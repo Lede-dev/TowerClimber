61bc09ff51ceae71d230206eaa2479f5c1377619
Lede-dev <lede.7716@gmail.com>
2024-01-26 19:57:04 +0900

    Initial commit

---
+++ Source/TowerClimber/Public/Character/PlatformerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "PlatformerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERCLIMBER_API APlatformerCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
